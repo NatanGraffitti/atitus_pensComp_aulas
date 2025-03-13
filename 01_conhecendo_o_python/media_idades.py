e1fbb0f50ba21ed6da565a7f972462f423b3280d
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-03-12 22:21:37 -0300

    Update media_idades.py

--- 01_conhecendo_o_python/media_idades.py
+++ 01_conhecendo_o_python/media_idades.py
@@ -1,6 +1,9 @@
 idade_A = 19
 idade_B = 19
 idade_C = 21
-media = (idade_B + idade_A + idade_C) / 3
+idade_D = 52
+idade_E = 24
+
+media = (idade_B + idade_A + idade_C + idade_D + idade_E) / 5
 
 print("A média é", media)
