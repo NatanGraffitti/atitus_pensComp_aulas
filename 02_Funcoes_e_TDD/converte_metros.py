93ba4ae3d2f6b8dcc53b214eb67faf81d391c428
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-03-17 22:21:02 -0300

    Update converte_metros.py

--- 02_Funcoes_e_TDD/converte_metros.py
+++ 02_Funcoes_e_TDD/converte_metros.py
@@ -1,17 +1,39 @@
-def metro_para_centimetro(valor):
-    return valor * 100
+def hora_para_minuto(valor):
+    return valor * 60
 
 
-def centimetro_para_metro(valor):
-    return valor / 100
+def minuto_para_segundo(valor):
+    return valor * 60
 
 
-assert metro_para_centimetro(1) == 100
-assert metro_para_centimetro(10) == 1000
-assert metro_para_centimetro(0) == 0
+def hora_para_segundo(valor):
+    return hora_para_minuto(valor * 60)
 
-assert centimetro_para_metro(100) == 1
-assert centimetro_para_metro(1000) == 10
-assert centimetro_para_metro(0) == 0
+def dia_para_segundo(valor):
+    return dia_para_segundo(valor * 24 * 60 * 60)
+
+def dia_para_horas(valor):
+    return dia_para_horas(valor * 24)
+
+
+assert hora_para_minuto(0) == 0
+assert hora_para_minuto(1) == 60
+assert hora_para_minuto(2) == 120
+
+assert minuto_para_segundo(0) == 0
+assert minuto_para_segundo(1) == 60
+assert minuto_para_segundo(2) == 120
+
+assert hora_para_segundo(0) == 0
+assert hora_para_segundo(1) == 3600  # 1 * 60 * 60
+assert hora_para_segundo(2) == 7200  # 2 * 60 * 60
+
+assert dia_para_segundo(0) == 0
+assert dia_para_segundo(1) == 86400
+assert dia_para_segundo(2) == 172800
+
+assert dia_para_horas(0) == 0
+assert dia_para_horas(1) == 24
+assert dia_para_horas(2) == 48
 
 print("Terminou com sucesso!")
