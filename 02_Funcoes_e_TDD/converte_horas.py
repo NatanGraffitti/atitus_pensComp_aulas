6ceecd9410609e1bab173578cd6d73a6b1034250
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-03-19 19:24:44 -0300

    Update converte_horas.py

--- 02_Funcoes_e_TDD/converte_horas.py
+++ 02_Funcoes_e_TDD/converte_horas.py
@@ -14,24 +14,24 @@ def dia_para_horas(valor):
     return dia_para_horas(valor * 24)
 
 
-assert hora_para_minuto(0) == 0
-assert hora_para_minuto(1) == 60
-assert hora_para_minuto(2) == 120
-
-assert minuto_para_segundo(0) == 0
-assert minuto_para_segundo(1) == 60
-assert minuto_para_segundo(2) == 120
-
-assert hora_para_segundo(0) == 0
-assert hora_para_segundo(1) == 3600  # 1 * 60 * 60
-assert hora_para_segundo(2) == 7200  # 2 * 60 * 60
-
-assert dia_para_segundo(0) == 0
-assert dia_para_segundo(1) == 86400
-assert dia_para_segundo(2) == 172800
-
-assert dia_para_horas(0) == 0
-assert dia_para_horas(1) == 24
-assert dia_para_horas(2) == 48
+    assert hora_para_minuto(0) == 0
+    assert hora_para_minuto(1) == 60
+    assert hora_para_minuto(2) == 120
+    
+    assert minuto_para_segundo(0) == 0
+    assert minuto_para_segundo(1) == 60
+    assert minuto_para_segundo(2) == 120
+    
+    assert hora_para_segundo(0) == 0
+    assert hora_para_segundo(1) == 3600  # 1 * 60 * 60
+    assert hora_para_segundo(2) == 7200  # 2 * 60 * 60
+    
+    assert dia_para_segundo(0) == 0
+    assert dia_para_segundo(1) == 86400
+    assert dia_para_segundo(2) == 172800
+    
+    assert dia_para_horas(0) == 0
+    assert dia_para_horas(1) == 24
+    assert dia_para_horas(2) == 48
 
 print("Terminou com sucesso!")
