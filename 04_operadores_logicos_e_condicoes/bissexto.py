935831916f5541658b99787e3c9dfbd79c24dd36
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-03-26 20:18:11 -0300

    Update bissexto.py

--- 04_operadores_logicos_e_condicoes/bissexto.py
+++ 04_operadores_logicos_e_condicoes/bissexto.py
@@ -1,12 +1,18 @@
 def eh_bissexto(ano): #função que se o ano for divisível por 4 retorna que o ano é bissexto
     if ano % 4 == 0:
-        return True
-    else: 
-        return False
-
-def proximo_bissexto(ano):
-    
-
+        return True and print ("Bissexto") #não precisa do return false pois o boolean ja vai retornar false
+    else:
+        return False and print ("Não bissexto")
+        
+def proximo_bissexto(ano): #return (ano + 3) // 4 * 4 -> solução em uma linha da correção em aula
+    if ano % 4 == 0:
+        return ano
+    if ano % 4 == 1:
+        return ano + 3
+    if ano % 4 == 2:
+        return ano + 2
+    if ano % 4 == 3:
+        return ano + 1
 
 def test():
     assert eh_bissexto(0)
@@ -22,5 +28,5 @@ def test():
     assert proximo_bissexto(2029) == 2032
     assert proximo_bissexto(2020) == 2020
 
-print (eh_bissexto(1999))
-
+print (eh_bissexto(23))
+print (proximo_bissexto(23))
