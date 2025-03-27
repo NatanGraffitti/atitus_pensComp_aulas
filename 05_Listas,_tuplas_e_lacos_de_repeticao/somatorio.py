96a652eb68f9ee2aa808f82b47d63d2e89d4b9d7
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-03-26 22:40:01 -0300

    resolver

---
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/somatorio.py
@@ -0,0 +1,24 @@
+def somatorio(numero):
+    if numero < 0:
+        return None
+
+    resultado = 0
+    
+    while resultado in range(10):
+        print (total, resultado)
+    return resultado
+    
+    
+   
+def test():    
+    assert somatorio(-1) is None
+    assert somatorio(0) == 0
+    assert somatorio(1) == 1
+    assert somatorio(2) == 3
+    assert somatorio(3) == 6
+    assert somatorio(4) == 10
+    assert somatorio(5) == 15
+    assert somatorio(6) == 21
+    assert somatorio(7) == 28
+    assert somatorio(8) == 36
+    assert somatorio(9) == 45
