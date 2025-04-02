0b9245b3caefc9ac36e9776ded52913d05f3e4db
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-04-02 17:28:34 -0300

    Update fatorial.py

--- 05_Listas,_tuplas_e_lacos_de_repeticao/fatorial.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/fatorial.py
@@ -1,12 +1,21 @@
 def fatorial(numero):
-    # Preencher
-    pass
+    if numero < 0:
+        return None
+    elif numero == 0:
+        return 1
+    else:
+        resultado = 1
+        for i in range(1, numero + 1):
+            resultado *= i
+        return resultado
+numero = 5
+print("o fatorial de ", numero, " é ", fatorial(numero)) 
 
-
-assert fatorial(0) == 1
-assert fatorial(1) == 1
-assert fatorial(2) == 2
-assert fatorial(3) == 6
-assert fatorial(4) == 24
-assert fatorial(5) == 120
-assert fatorial(-1) is None
+def test():
+    assert fatorial(0) == 1
+    assert fatorial(1) == 1
+    assert fatorial(2) == 2
+    assert fatorial(3) == 6
+    assert fatorial(4) == 24
+    assert fatorial(5) == 120
+    assert fatorial(-1) is None
