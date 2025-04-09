b7f72295f79e47a31d3583a95c8443e5657f110c
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-04-09 16:43:01 -0300

    Update baskhara.py

--- 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
@@ -1,5 +1,16 @@
-def baskhara(a, b, c):
-    
+def bhaskara(a, b, c):
+    delta = b**2 - 4 * a * c  # Calcula o delta
+
+    if delta < 0:
+        return "Sem raízes reais"
+    elif delta == 0:
+        x = -b / (2 * a)
+        return (x, x)
+    else:
+        raiz_delta = raiz_quadrada(delta)  
+        x1 = (-b + raiz_delta) / (2 * a)  # Calcula a primeira raiz
+        x2 = (-b - raiz_delta) / (2 * a)  # Calcula a segunda raiz
+        return (x1, x2)  # Retorna as duas raízes
 
 
 def test():
@@ -10,3 +21,6 @@ def test():
     
     assert baskhara(1, 2, 3) is None
     assert baskhara(1, 0, 0) == 0
+
+
+baskhara (2, 3, -2)
