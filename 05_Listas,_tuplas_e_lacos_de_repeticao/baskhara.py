76007330b2f84686a72ef4e030b47dfd30eb7a7c
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-04-10 15:51:51 -0300

    Update baskhara.py

--- 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
@@ -1,26 +1,23 @@
-def raiz_quadrada(numero):
-    return numero ** 0.5
-
 def baskhara(a, b, c):
-    delta = b**2 - 4 * a * c
+  discriminante = b**2 - 4*a*c
 
-    if delta < 0:
-        return None  
-    elif delta == 0:
-        x = -b / (2 * a)
-        return [x, x]
+  while True:
+    if discriminante < 0:
+        return False
     else:
-        raiz_delta = raiz_quadrada(delta)
-        x1 = (-b + raiz_delta) / (2 * a)
-        x2 = (-b - raiz_delta) / (2 * a)
-        return [x1, x2]
+        return True
 
-def test():
-    assert baskhara(1, -3, 2) == [2, 1]
-    assert baskhara(2, 3, -2) == [0.5, -2] 
-    assert baskhara(1, -5, 6) == [3, 2] 
-    assert baskhara(1, -7, 10) == [5, 2] 
-    assert baskhara(1, 2, 3) is None
-    assert baskhara(1, 0, 0) == [0, 0] 
+    if discrimante = 0:
+        return "Não é possível realizar a equação"
 
-print(baskhara(2, 3, -2))
+    x1 = (-b + ((b**2 - 4*a*c) * 1/2)) / (2 * a)
+    x2 = (-b - ((b**2 - 4*a*c) * 1/2)) / (2 * a)
+
+def test ():
+  assert baskhara(1, -3, 2) == [2, 1]
+  assert baskhara(2, 3, -2) == [-2, 0.5]
+  assert baskhara(1, -5, 6) == [2, 3]
+  assert baskhara(1, -7, 10) == [2, 5]
+
+  assert baskhara(1, 2, 3) is None
+  assert baskhara(1, 0, 0) == 0
