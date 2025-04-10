8f0a309eedb1e8a3d403fa3341641f1bc6919c23
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-04-10 16:52:09 -0300

    Update descobre_senha.py

--- 06_Listas_continuacao/descobre_senha.py
+++ 06_Listas_continuacao/descobre_senha.py
@@ -1,16 +1,16 @@
-segredo = 5
-tentativas = 0
-palpites = [3, 8, 5]  # Lista de palpites predefinidos
+def segredo(valor):
+    if valor < 0 or valor > 10:
+        return False
+    tentativas = 1
+    while (valor):
+        numero = int(input('digite um numero entre 0 e 10:'))
+        if numero < 0 or numero > 10:
+            print('Valor invalido, tente novamente')
+        if numero == valor:
+            print('Segredo descoberto', tentativas, 'tentativas')
+            return True
+        tentativas += 1
 
-for palpite in palpites:
-    tentativas += 1
-    print(f"Tentativa {tentativas}: Palpite = {palpite}")  # Adicionado para depuração
-    if palpite == segredo:
-        print('Parabéns você acertou o segredo em', tentativas, 'tentativas!')
-        break  # Sai do loop quando o palpite correto é encontrado
-    else:
-        print('Errado, tente novamente!')
 
-if palpite != segredo:
-    print('Você não acertou o número secreto.')
-    print(f"O número secreto era: {segredo}")
+
+print(segredo(0))
