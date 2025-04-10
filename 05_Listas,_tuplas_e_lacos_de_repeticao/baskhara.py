d7807a1db1e1f363fb20e34ac462641fac64a911
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-04-10 16:16:15 -0300

    Update baskhara.py

--- 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
@@ -1,22 +1,22 @@
-def baskhara(a, b, c):
-    delta = (b**2) - ((4*a)*c)
-    if delta < 0:
-        return None
-    if delta == 0:
-        return x1 or x2
-    x1 = (-b + delta**(1/2)) / (2 * a)
-    x2 = (-b -delta**(1/2)) / (2 * a)
-    return [x1,x2]
+#def baskhara(a, b, c):
+#    delta = (b**2) - ((4*a)*c)
+#    if delta < 0:
+#        return None
+#    if delta == 0:
+#        return x1 or x2
+#    x1 = (-b + delta**(1/2)) / (2 * a)
+#    x2 = (-b -delta**(1/2)) / (2 * a)
+#    return [x1,x2]
     # retorne apenas um valor se discriminante == 0
     # retorne [x1, x2] nos outros casos
-    pass
+#    pass
 
-def test():
-    assert baskhara(1, -3, 2) == [2, 1]
-    assert baskhara(2, 3, -2) == [-2, 0.5]
-    assert baskhara(1, -5, 6) == [2, 3]
-    assert baskhara(1, -7, 10) == [2, 5]
-    assert baskhara(1, 2, 3) is None
-    assert baskhara(1, 0, 0) == 0
+#def test():
+#    assert baskhara(1, -3, 2) == [2, 1]
+#    assert baskhara(2, 3, -2) == [-2, 0.5]
+#    assert baskhara(1, -5, 6) == [2, 3]
+#    assert baskhara(1, -7, 10) == [2, 5]
+#    assert baskhara(1, 2, 3) is None
+#    assert baskhara(1, 0, 0) == 0
 
-print(baskhara(1, -3, 2))
+# print(baskhara(1, -3, 2))
