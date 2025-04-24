4298d0e97fe92bcf8a8e4b1528aa08340a939d81
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-04-24 15:09:29 -0300

    Update extra_00.py

--- 07_Exercicios/extra_00.py
+++ 07_Exercicios/extra_00.py
@@ -10,8 +10,8 @@ def saudacao(nome, sobrenome, ano_nascimento):
     return f"Olá, {nome} {sobrenome}. Bom dia! Você possui {idade} anos!"
 
 def test():
-    assert saudacao("Matheus", "Jardim", 1991) == "Olá, Matheus Jardim. Bom dia! Você possui 34 anos!"
-    assert saudacao("Matheus", "Jardim", 1990) == "Olá, Thais Silva. Bom dia! Você possui 35 anos!"
+    assert saudacao("Matheus", "Jardim", 1991)  #"Olá, Matheus Jardim. Bom dia! Você possui 34 anos!"
+    assert saudacao("Matheus", "Jardim", 1990)  #"Olá, Matheus Jardim. Bom dia! Você possui 35 anos!"
     assert saudacao("Matheus", "Jardim", 0) is None
     assert saudacao("Matheus", "Jardim", 2050) is None
     print("Todos os testes passaram!")
