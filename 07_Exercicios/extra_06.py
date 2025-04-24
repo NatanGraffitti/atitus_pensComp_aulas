b915d6b739e7a4a00c2ab8b3977e5875ca1b9808
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-04-24 15:07:12 -0300

    Update extra_06.py

--- 07_Exercicios/extra_06.py
+++ 07_Exercicios/extra_06.py
@@ -7,12 +7,11 @@ def valor_pgto(valor, forma_pgto):
         return valor
     if forma_pgto == 4:
         return valor + (valor * (10 / 100))
-    if forma_pagto not in [1, 2, 3, 4]:
-        return "Opção inválida. Por favor, escolha uma das opções listadas.")
+    if forma_pgto not in [1, 2, 3, 4]:
+        return "Opção inválida. Por favor, escolha uma das opções listadas."
 
 def test():
     assert valor_pgto(100, 1) == 85
     assert valor_pgto(100, 2) == 90
     assert valor_pgto(100, 3) == 100
     assert valor_pgto(100, 4) == 110
-    assert isinstance(valor_pgto(100, 5), str)
