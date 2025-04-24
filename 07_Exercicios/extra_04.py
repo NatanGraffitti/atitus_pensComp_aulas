7221dcacceda2aed9e12cdf88fc716652e67d6db
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-04-24 15:45:06 -0300

    Update extra_04.py

--- 07_Exercicios/extra_04.py
+++ 07_Exercicios/extra_04.py
@@ -28,10 +28,10 @@ print("A família é da", classe_da_familia)
 
 def test():
     assert calcula_classe_social([], 1000) is None  
-    assert calcula_classe_social([1000], 1000) == "E"  
+    assert calcula_classe_social([1000], 1000) == "D"  
     assert calcula_classe_social([500], 1000) == "E"  
     assert calcula_classe_social([1000, 0], 900) == "E"  
-    assert calcula_classe_social([1000], 900) == "D" 
+    assert calcula_classe_social([1000], 900) == "D"  
     assert calcula_classe_social([10000, 15000], 1000) == "B"
     assert calcula_classe_social([20000, 25000], 1000) == "A"
     assert calcula_classe_social([20000, 0, 0, 0, 0], 1000) == "C"
