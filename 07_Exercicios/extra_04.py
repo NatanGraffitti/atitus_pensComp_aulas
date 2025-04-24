f586ebb302daa07901d582dcddd1a1bd4b2a36dd
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-04-24 15:55:59 -0300

    Update extra_04.py

--- 07_Exercicios/extra_04.py
+++ 07_Exercicios/extra_04.py
@@ -1,37 +1,28 @@
 def calcula_classe_social(salarios, salario_minimo):
-    if len(salarios) == 0:  # Verifica se a lista de salários está vazia
+    if not salarios:  
         return None
-
-    total_dinheiro = sum(salarios)
-    total_pessoas = len(salarios)
-    renda_por_pessoa = total_dinheiro / total_pessoas
-    salarios_minimos_por_pessoa = renda_por_pessoa / salario_minimo
-
-    if salarios_minimos_por_pessoa > 15:
-        classe = "Classe A"
-    elif salarios_minimos_por_pessoa >= 5:
-        classe = "Classe B"
-    elif salarios_minimos_por_pessoa >= 3:
-        classe = "Classe C"
-    elif salarios_minimos_por_pessoa >= 1:
-        classe = "Classe D"
-    else:
-        classe = "Classe E"
     
-    return classe
-
-salarios_da_familia = [2000, 1500, 800, 500]
-valor_salario_minimo = 1412.00  
-
-classe_da_familia = calcula_classe_social(salarios_da_familia, valor_salario_minimo)
-print("A família é da", classe_da_familia)
+    total_rendimento = sum(salarios)  
+    
+   
+    if total_rendimento <= salario_minimo:
+        return "E"
+    if total_rendimento <= 3 * salario_minimo:
+        return "D"
+    if total_rendimento <= 10 * salario_minimo:
+        return "C"
+    if total_rendimento <= 20 * salario_minimo:
+        return "B"
+    else:
+        return "A"
+    
 
 def test():
-    assert calcula_classe_social([], 1000) is None  
-    assert calcula_classe_social([1000], 1000) == "D"  
-    assert calcula_classe_social([500], 1000) == "E"  
-    assert calcula_classe_social([1000, 0], 900) == "E"  
-    assert calcula_classe_social([1000], 900) == "D"  
+    assert calcula_classe_social([], 1000) is None
+    assert calcula_classe_social([1000], 1000) == "E"
+    assert calcula_classe_social([500], 1000) == "E"
+    assert calcula_classe_social([1000, 0], 900) == "E"
+    assert calcula_classe_social([1000], 900) == "D"
     assert calcula_classe_social([10000, 15000], 1000) == "B"
     assert calcula_classe_social([20000, 25000], 1000) == "A"
     assert calcula_classe_social([20000, 0, 0, 0, 0], 1000) == "C"
