98b3b301c3dccccaf1756127187e4c4f19bf7627
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-04-24 14:29:04 -0300

    Update extra_00.py

--- 07_Exercicios/extra_00.py
+++ 07_Exercicios/extra_00.py
@@ -1,20 +1,20 @@
-def saudacao():
+def saudacao(nome, sobrenome, ano_nascimento):
     ANO_ATUAL = 2025
-    nome = input('Qual o seu nome?')
-    sobrenome = input('Qual o seu sobrenome?')
-    data_de_nascimento = int(input('Qual o seu ano de nascimento?'))
-    idade = ANO_ATUAL - data_de_nascimento 
 
-print(f'Olá,', '" nome, sobrenome "', 'você tem: ', idade, 'anos')
+    # O ano de nascimento precisa ser maior que zero e não pode ser no futuro
+    if ano_nascimento <= 0 or ano_nascimento > ANO_ATUAL:
+        return None
+        
+    idade = ANO_ATUAL - ano_nascimento
+    
+    return f"Olá, {nome} {sobrenome}. Bom dia! Você possui {idade} anos!"
 
 def test():
-assert (
-    saudacao("Matheus", "Jardim", 1991)
-    == "Olá, Matheus Jardim. Bom dia! Você possui 33 anos!"
-)
-assert (
-    saudacao("Thais", "Silva", 1990)
-    == "Olá, Thais Silva. Bom dia! Você possui 34 anos!"
-)
-assert saudacao("Matheus", "Jardim", 0) is None
-assert saudacao("Matheus", "Jardim", 2050) is None
+    assert saudacao("Matheus", "Jardim", 1991) == "Olá, Matheus Jardim. Bom dia! Você possui 34 anos!"
+    assert saudacao("Matheus", "Jardim", 1990) == "Olá, Thais Silva. Bom dia! Você possui 35 anos!"
+    assert saudacao("Matheus", "Jardim", 0) is None
+    assert saudacao("Matheus", "Jardim", 2050) is None
+    print("Todos os testes passaram!")
+
+if __name__ == "__main__":
+    print(saudacao("Matheus", "Jardim", 2000)) 
