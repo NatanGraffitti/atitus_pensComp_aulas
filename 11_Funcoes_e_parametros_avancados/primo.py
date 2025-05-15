277ab8149513d6ba759433fe0e5ca3f6c729ccfa
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-05-14 21:33:37 -0300

    Update primo.py

--- 11_Funcoes_e_parametros_avancados/primo.py
+++ 11_Funcoes_e_parametros_avancados/primo.py
@@ -1,11 +1,18 @@
 def eh_primo(numero: int) -> bool:
-    pass
+    if numero <= 1:
+        return False
 
+    for i in range(2, numero): #[2 ... 6] -> lista de dois ate numero sem incluir numero
+        if numero % i ==0:
+            return False
 
-assert not eh_primo(-1)
-assert not eh_primo(0)
-assert not eh_primo(1)
-assert eh_primo(2)
-assert eh_primo(3)
-assert not eh_primo(4)
-assert eh_primo(5)
+    return True
+
+def test():
+    assert not eh_primo(-1)
+    assert not eh_primo(0)
+    assert not eh_primo(1)
+    assert eh_primo(2)
+    assert eh_primo(3)
+    assert not eh_primo(4)
+    assert eh_primo(5)
