41e7c05f7af1588a23c590eee8412f32d443f96b
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-05-21 13:28:09 -0300

    Update mult_matriz.py

--- 06_Listas_continuacao/mult_matriz.py
+++ 06_Listas_continuacao/mult_matriz.py
@@ -6,7 +6,7 @@ def multiplica_matrix_scalar(matrix, scalar):
             resultado_linha.append(elemento * scalar)
         resultado.append(resultado_linha)
     return resultado
-    
+
 matrix = [[1, 2], [3, 4]]
 scalar = 2
 result = multiplica_matrix_scalar(matrix, scalar)
@@ -16,8 +16,10 @@ print(result)
 def test():
     matrix_1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     result_01 = [[3, 6, 9], [12, 15, 18], [21, 24, 27]]
-    assert multiply_matrix_by_scalar(matrix_1, 3) == result_01
+    assert multiplica_matrix_scalar(matrix_1, 3) == result_01  # Corrigido o nome da função
 
     matrix_02 = [[2, 3, 4], [5, 6, 7], [8, 9, 10]]
     result_02 = [[8, 12, 16], [20, 24, 28], [32, 36, 40]]
-    assert multiply_matrix_by_scalar(matrix_02, 4) == result_02
+    assert multiplica_matrix_scalar(matrix_02, 4) == result_02  # Corrigido o nome da função
+
+
