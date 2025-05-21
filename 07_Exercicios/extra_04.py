7334cad07c1d823e68a5f5a21780bf1287ebf444
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-05-21 13:47:28 -0300

    Update extra_04.py

--- 07_Exercicios/extra_04.py
+++ 07_Exercicios/extra_04.py
@@ -19,7 +19,7 @@ def test():
     assert calcula_classe_social([], 1000) is None
     assert calcula_classe_social([1000], 1000) == "E"
     assert calcula_classe_social([500], 1000) == "E"
-    assert calcula_classe_social([1000, 0], 900) == "E"
+    assert calcula_clte_social([1000, 0], 900) == "E"
     assert calcula_classe_social([1000], 900) == "D"
     assert calcula_classe_social([10000, 15000], 1000) == "B"
     assert calcula_classe_social([20000, 25000], 1000) == "A"
