c8a3e1ce9912f57dda6403834da6e0b335d5ce17
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-05-21 15:45:26 -0300

    Update dvd_animation.py

--- 06_Listas_continuacao/dvd_animation.py
+++ 06_Listas_continuacao/dvd_animation.py
@@ -1,8 +1,8 @@
-def limpa_tela():
-    import os
+def limpa_tela(): #DVD Animação
+    import os  
 
     os.system("cls" if os.name == "nt" else "clear")  # Limpa o console
-
+ 
 
 def espera():
     import time
@@ -10,9 +10,37 @@ def espera():
     time.sleep(0.5)  # Espera meio segundo (500ms)
 
 
-def desenha_tela():
-    # Preencher
-    pass
+def desenha_tela(altura, largura, x, y, colisoes):
+    limpa_tela()
+    for i in range(altura):
+        for j in range(largura):
+            if i == 0 or i == altura - 1 or j == 0 or j == largura - 1:
+                print("#", end="") 
+            elif i == y and j == x:
+                print("A", end="") 
+            else:
+                print(".", end="") 
+        print()
+    print(f"Colisões: {colisoes}")
 
+altura = 10
+largura = 20
+x = 1
+y = 1
+colisoes = 0
+direcao_x = 1
+direcao_y = 1
 
-desenha_tela()
+while True:
+    desenha_tela(altura, largura, x, y, colisoes)
+    espera()
+
+    x += direcao_x
+    y += direcao_y
+
+    if x == 0 or x == largura - 1:
+        direcao_x *= -1
+        colisoes += 1
+    if y == 0 or y == altura - 1:
+        direcao_y *= -1
+        colisoes += 1
