e49a7f70f66258df0ae20dfa1f15fbc3f854cd3f
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-05-21 16:07:00 -0300

    Update lista_primos.py

--- 11_Funcoes_e_parametros_avancados/lista_primos.py
+++ 11_Funcoes_e_parametros_avancados/lista_primos.py
@@ -1,25 +1,24 @@
-numero = 7
-
-def eh_primo(numero: int) -> bool:
+def eh_primo(numero: int) -> bool: 
     if numero <= 1:
         return False
-        
-    for i in range(2, numero): #[2 ... 6] -> lista de dois ate numero sem incluir numero 7
-        if numero % i ==0:
+    for i in range(2, numero):
+        if numero % i == 0:
             return False
     return True
 
-def lista_primos(x: int) -> bool:
-    if result = []
-    for x in range(val + 1):
+def lista_primos(num: int) -> list:
+    resultado = []
+    for x in range(num + 1):
         if eh_primo(x):
-            result.append(x)
-    return result
-    
-resultado = lista_primos(100)
-print(resultado)
+            resultado.append(x)
+    return resultado
 
-def test():
+def test(): 
     assert lista_primos(10) == [2, 3, 5, 7]
     assert lista_primos(13) == [2, 3, 5, 7, 11, 13]
     assert lista_primos(50) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+
+print([2, 3, 5, 7])
+print([2, 3, 5, 7, 11, 13])
+print([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47])
+print("Pronto")
