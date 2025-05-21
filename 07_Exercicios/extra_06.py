859e5d3c4997e8482d49c97276c680dc863a9768
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-05-21 14:26:32 -0300

    Update extra_06.py

--- 07_Exercicios/extra_06.py
+++ 07_Exercicios/extra_06.py
@@ -1,17 +1,29 @@
 def valor_pgto(valor, forma_pgto):
     if forma_pgto == 1:
-        return valor - (valor * (15 / 100))
-    if forma_pgto == 2:
-        return valor - (valor * (10 / 100))
-    if forma_pgto == 3:
+        resultado = valor - (valor * (15 / 100))
+        print(f"Valor com desconto de 15%: {resultado}")
+        return resultado
+    elif forma_pgto == 2:
+        resultado = valor - (valor * (10 / 100))
+        print(f"Valor com desconto de 10%: {resultado}")
+        return resultado
+    elif forma_pgto == 3:
+        print(f"Parcelado em 2x sem juros. Valor total {valor}")
         return valor
-    if forma_pgto == 4:
-        return valor + (valor * (10 / 100))
-    if forma_pgto not in [1, 2, 3, 4]:
-        return "Opção inválida. Por favor, escolha uma das opções listadas."
+    elif forma_pgto == 4:
+        resultado = valor + (valor * (10 / 100))
+        print(f"Valor com acréscimo de 10%: {resultado}")
+        return resultado
+    else:
+        print("Opção inválida. Por favor, escolha uma das opções listadas.")
+        return None
 
+# Função de teste
 def test():
     assert valor_pgto(100, 1) == 85
     assert valor_pgto(100, 2) == 90
     assert valor_pgto(100, 3) == 100
     assert valor_pgto(100, 4) == 110
+    print("Todos os testes passaram.")
+
+valor_pgto(100, 2)
