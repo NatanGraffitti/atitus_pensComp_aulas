9319d61ded8932ffd373e1510865d5ff7bc7d601
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-05-21 14:52:28 -0300

    Update extra_06.py

--- 07_Exercicios/extra_06.py
+++ 07_Exercicios/extra_06.py
@@ -1,29 +1,30 @@
 def valor_pgto(valor, forma_pgto):
-    if forma_pgto == 1:
-        resultado = valor - (valor * (15 / 100))
-        print(f"Valor com desconto de 15%: {resultado}")
-        return resultado
-    elif forma_pgto == 2:
-        resultado = valor - (valor * (10 / 100))
-        print(f"Valor com desconto de 10%: {resultado}")
-        return resultado
-    elif forma_pgto == 3:
-        print(f"Parcelado em 2x sem juros. Valor total {valor}")
-        return valor
-    elif forma_pgto == 4:
-        resultado = valor + (valor * (10 / 100))
-        print(f"Valor com acréscimo de 10%: {resultado}")
-        return resultado
-    else:
-        print("Opção inválida. Por favor, escolha uma das opções listadas.")
-        return None
+  valor = float(input("Digite o valor a ser pago:")
+  print("Formas de pagamento: ")
+  print("1 - para PIX")
+  print("2 - Para À Vista")
+  print("3 - Parcelado em 2x sem juros")
+  print("4 - Parcelado em 3x ou mais com juros")
+  pagamento = int(input("Digite o número relacionado com a forma de pagamento que sera realizada."))
 
-# Função de teste
-def test():
-    assert valor_pgto(100, 1) == 85
-    assert valor_pgto(100, 2) == 90
-    assert valor_pgto(100, 3) == 100
-    assert valor_pgto(100, 4) == 110
-    print("Todos os testes passaram.")
+  if pagamento = 1:
+      resultado = valor - (valor * (15 / 100))
+      print (f"Valor com desconto de 15%: {resultado}")
+  elif pagamento == 2:
+      resultado = valor - (valor * (10 / 100))
+      print(f"Valor com desconto de 10%: {resultado}")
+  elif pagamento == 3:
+      print(f"Parcelado em 2x sem juros. Valor total {valor}")
+  elif pagamento == 4:
+      resultado = valor + (valor * (10 / 100))
+      print(f"Valor com acréscimo de 10%: {resultado}")
+  else:
+    print("Opção inválida. Por favor, escolha uma das opções listadas.")
 
-valor_pgto(100, 2)
+
+
+def test ():
+  assert valor_pgto(100, 1) == 85
+  assert valor_pgto(100, 2) == 90
+  assert valor_pgto(100, 3) == 100
+  assert valor_pgto(100, 4) == 110
