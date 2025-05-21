882ef4303ec1d6719a0b10db6091c241bfb9e55e
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-05-21 16:11:23 -0300

    Update soma_matriz.py

--- 11_Funcoes_e_parametros_avancados/soma_matriz.py
+++ 11_Funcoes_e_parametros_avancados/soma_matriz.py
@@ -1,12 +1,19 @@
 def soma_matriz(matriz, alvo):
-    pass
+    soma = 0
 
+    for linha in matriz:
+        for valor in linha:
+            if valor > 0 and valor % 2 == 0:
+                soma += valor
 
+    return soma == alvo
+     
 matriz = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9],
 ]
+
 assert soma_matriz(matriz, 20)
 assert not soma_matriz(matriz, 18)
 assert not soma_matriz(matriz, 21)
