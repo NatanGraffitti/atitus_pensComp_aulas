67992da6bada2bd85d6a6667f71e983e4a339650
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-05-21 16:15:06 -0300

    Update validador_parenteses.py

--- 11_Funcoes_e_parametros_avancados/validador_parenteses.py
+++ 11_Funcoes_e_parametros_avancados/validador_parenteses.py
@@ -1,6 +1,17 @@
 def validador_parenteses(entrada: str) -> bool:
-    pass
+    parenteses_a = '' 
+    parenteses_b = ''
 
+    for x in entrada:
+        if x == '(':
+            parenteses_a += x
+        else:
+            parenteses_b += x
+
+    if len(parenteses_a) == len(parenteses_b):
+        return True
+    else:
+        return False
 
 # Valores válidos
 assert validador_parenteses("()")
