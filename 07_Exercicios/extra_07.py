71861eb1fc2c8623462ca33df648cea43ed3a51f
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-05-21 14:43:57 -0300

    Update extra_07.py

--- 07_Exercicios/extra_07.py
+++ 07_Exercicios/extra_07.py
@@ -1,15 +1,20 @@
 def fahrenheit_para_celsius(valor):
-    return (valor - 32) / 1.8
+  temp = float(input("Diga a temperatura em Fahrenheit: "))
+  f_para_c = (temp - 32) / 1.8
+  print(f"A temperatura em graus Celcius é:  {f_para_c}C°")
 
 
-def celsius_para_fahrenheit(valor):
-    return (1.8 * valor) + 32
+def celsius_para_farenheit(valor):
+  temp = float(input("Diga a temperatura em Celsius: "))
+  c_para_f = (1.8 * temp) + 32
+  print(f"A temperatura em graus Fahrenheit é: {c_para_f}F°")
 
-def test():
+
+def testar():
     assert fahrenheit_para_celsius(104) == 40
     assert fahrenheit_para_celsius(-13) == -25
-
     assert celsius_para_fahrenheit(40) == 104
     assert celsius_para_fahrenheit(-25) == -13
-
     assert celsius_para_fahrenheit(fahrenheit_para_celsius(30)) == 30
+
+fahrenheit_para_celsius(104)
