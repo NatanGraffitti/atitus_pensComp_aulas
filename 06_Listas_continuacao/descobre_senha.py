92eae6e60b4b158fd56afa3f0a67a3c89845ddfc
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-05-21 15:42:25 -0300

    Update descobre_senha.py

--- 06_Listas_continuacao/descobre_senha.py
+++ 06_Listas_continuacao/descobre_senha.py
@@ -1,16 +1,15 @@
-def segredo(valor):
-    if valor < 0 or valor > 10:
-        return False
-    tentativas = 1
-    while (valor):
-        numero = int(input('digite um numero entre 0 e 10:'))
-        if numero < 0 or numero > 10:
-            print('Valor invalido, tente novamente')
-        if numero == valor:
-            print('Segredo descoberto', tentativas, 'tentativas')
-            return True
+def descobre_senha(senha_secreta, total_tentativas): 
+    tentativas = 0
+    for tentativa in total_tentativas:
+        if tentativa < 1 or tentativa > 10:
+            continue
         tentativas += 1
+        if tentativa == senha_secreta:
+            return tentativas  
+    return -1  
 
-
-
-print(segredo(0))
+def test_descobre_senha():
+    assert descobre_senha(6, [6]) == 1
+    assert descobre_senha(6, [0, 11, 3, 6]) == 2  
+    assert descobre_senha(6, [1, 2, 3, 4, 5]) == -1
+    assert descobre_senha(6, [1, 2, 3, 4, 5, 6]) == 6
