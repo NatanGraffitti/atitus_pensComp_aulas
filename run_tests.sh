2a729cda7b85fdf66380447dede05838ecd8cb05
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-06-17 17:38:32 -0300

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -19,13 +19,13 @@ run_tests() {
  run_tests 09_Dicionários/*.py
  run_tests 10_Introducao_a_funcoes/*.py
  run_tests 11_Funcoes_e_parametros_avancados/*.py
-# run_tests 12_Funcoes_e_imports/*.py
-# run_tests 13_exercicios_leetcode/*.py
-# run_tests 14_manipulacao_de_datas/*.py
-# run_tests 15_manipulacao_de_arquivos/*.py
-# run_tests 16_json,XML,CSV/*.py
-# run_tests 17_TDD/*.py
-# run_tests 18_exercicios/*.py
-# run_tests XX_extras/*.py
+ run_tests 12_Funcoes_e_imports/*.py
+ run_tests 13_exercicios_leetcode/*.py
+ run_tests 14_manipulacao_de_datas/*.py
+ run_tests 15_manipulacao_de_arquivos/*.py
+ run_tests 16_json,XML,CSV/*.py
+ run_tests 17_TDD/*.py
+ run_tests 18_exercicios/*.py
+ run_tests XX_extras/*.py
 
 echo "All tests ran successfully"
