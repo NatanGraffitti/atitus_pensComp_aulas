d65e2f38bbf0484a6d6b696ed1cb06a88680f872
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-06-17 17:49:49 -0300

    Update op_multip.py

--- 12_Funcoes_e_imports/op_math/op_multip.py
+++ 12_Funcoes_e_imports/op_math/op_multip.py
@@ -1,6 +1,13 @@
 def multiplicacao(a: int, b: int) -> int:
-    # multiplicacao(a, b): não pode usar o símbolo ‘*’
-    pass
+    resultado = 0
+    negativo = (a < 0) != (b < 0)
+    a_abs = abs(a)
+    b_abs = abs(b)
+
+    for _ in range(b_abs):
+        resultado += a_abs
+
+    return -resultado if negativo else resultado
 
 
 assert multiplicacao(-10, 2) == -20
