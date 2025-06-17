18d9708a87b9078893f0212966f968ac8fe4df15
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-06-17 18:00:17 -0300

    Update busca_binaria_v1.py

--- 13_exercicios_leetcode/busca_binaria_v1.py
+++ 13_exercicios_leetcode/busca_binaria_v1.py
@@ -11,5 +11,6 @@ def busca_binaria(lista: list, valor: int) -> bool:
             direita = meio - 1
     return False
 
-assert busca_binaria([1, 3, 5, 7, 9, 11, 13, 15], 7)
-assert not busca_binaria([1, 3, 5, 7, 9, 11, 13, 15], 8)
+def test():
+    assert busca_binaria([1, 3, 5, 7, 9, 11, 13, 15], 7)
+    assert not busca_binaria([1, 3, 5, 7, 9, 11, 13, 15], 8)
