3ae489502fdf368d10dd237a9939aafb472424af
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-06-17 18:00:01 -0300

    Update anagrama.py

--- 13_exercicios_leetcode/anagrama.py
+++ 13_exercicios_leetcode/anagrama.py
@@ -4,10 +4,11 @@ def sao_anagramas(palavra1: str, palavra2: str) -> bool:
 
     return sorted(palavra1) == sorted(palavra2)
 
-assert sao_anagramas("amor", "roma")
-assert sao_anagramas("iracema", "america")
-assert sao_anagramas("estudo", "duetos")
-
-assert not sao_anagramas("banana", "anana")
-assert not sao_anagramas("banana", "")
-assert not sao_anagramas("banana", "abc")
+def test():
+    assert sao_anagramas("amor", "roma")
+    assert sao_anagramas("iracema", "america")
+    assert sao_anagramas("estudo", "duetos")
+    
+    assert not sao_anagramas("banana", "anana")
+    assert not sao_anagramas("banana", "")
+    assert not sao_anagramas("banana", "abc")
