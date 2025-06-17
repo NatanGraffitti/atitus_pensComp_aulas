af1bfb82064c1e5fe12a09ec82c6817364fa19db
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-06-17 17:59:35 -0300

    Update fibonacci.py

--- 12_Funcoes_e_imports/fibonacci.py
+++ 12_Funcoes_e_imports/fibonacci.py
@@ -9,12 +9,12 @@ def fibonacci(x):
 
 print(fibonacci(9))
 
-
-assert fibonacci(-1) is None
-assert fibonacci(0) == 0
-assert fibonacci(1) == 1
-assert fibonacci(2) == 1
-assert fibonacci(3) == 2
-assert fibonacci(4) == 3
-assert fibonacci(5) == 5
-assert fibonacci(10) == 55
+def test():
+    assert fibonacci(-1) is None
+    assert fibonacci(0) == 0
+    assert fibonacci(1) == 1
+    assert fibonacci(2) == 1
+    assert fibonacci(3) == 2
+    assert fibonacci(4) == 3
+    assert fibonacci(5) == 5
+    assert fibonacci(10) == 55
