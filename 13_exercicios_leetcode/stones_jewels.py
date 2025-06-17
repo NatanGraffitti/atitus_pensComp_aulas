8cac9efcda270b7bed3b7f0af4462b504ff6c10e
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-06-17 18:03:26 -0300

    Update stones_jewels.py

--- 13_exercicios_leetcode/stones_jewels.py
+++ 13_exercicios_leetcode/stones_jewels.py
@@ -1,9 +1,16 @@
 # https://leetcode.com/problems/jewels-and-stones/description/
 
+def stones_jewels(jewels, stones):
+    conjunto_joias = set(jewels)
+    contador = 0
+    for pedra in stones:
+        if pedra in conjunto_joias:
+            contador += 1
+    return contador
 
-def stones_jewels(stones, jewels):
-    pass
 
+def test():
+    assert stones_jewels(jewels="aA", stones="aAAbbbb") == 3
+    assert stones_jewels(jewels="z", stones="ZZ") == 0
 
-assert stones_jewels(jewels="aA", stones="aAAbbbb") == 3
-assert stones_jewels(jewels="z", stones="ZZ") == 0
+print("Testes OK!")
