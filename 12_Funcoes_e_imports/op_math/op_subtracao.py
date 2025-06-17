764a47c319ebacbd9f233c8c696dec7d65ea9ec0
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-06-17 17:58:55 -0300

    Update op_subtracao.py

--- 12_Funcoes_e_imports/op_math/op_subtracao.py
+++ 12_Funcoes_e_imports/op_math/op_subtracao.py
@@ -4,8 +4,9 @@ def negativo(x: int) -> int:
 def subtracao(valor1: int, valor2: int) -> int:
     return valor1 + negativo(valor2)
 
-assert subtracao(-10, 2) == -12
-assert subtracao(10, -2) == 12
-assert subtracao(-10, -2) == -14
-assert subtracao(10, 2) == 8
-assert subtracao(10, 0) == 10
+def test():
+    assert subtracao(-10, 2) == -12
+    assert subtracao(10, -2) == 12
+    assert subtracao(-10, -2) == -14
+    assert subtracao(10, 2) == 8
+    assert subtracao(10, 0) == 10
