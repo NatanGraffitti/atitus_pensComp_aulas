f950ae9ccd73f663b68d2f598ff057a63ce4eaae
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-06-17 17:48:55 -0300

    Update op_divisao.py

--- 12_Funcoes_e_imports/op_math/op_divisao.py
+++ 12_Funcoes_e_imports/op_math/op_divisao.py
@@ -1,7 +1,16 @@
 def divisao(a: int, b: int) -> int | None:
-    # divisao(a, b): não pode usar o símbolo ‘/’
-    pass
+    if b == 0:
+        return None  # divisão por zero não 
+    negativo = (a < 0) != (b < 0) 
 
+    a = abs(a)
+    b = abs(b)
+    
+    quociente = 0
+    while a >= b:
+        a -= b
+        quociente += 1  
+    return -quociente if negativo else quociente
 
 assert divisao(-10, 2) == -5
 assert divisao(10, -2) == -5
