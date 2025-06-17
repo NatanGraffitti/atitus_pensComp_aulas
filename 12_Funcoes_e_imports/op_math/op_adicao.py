6d0988f73a4697ac88963e0941c496c8ce1d9ade
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-06-17 17:47:40 -0300

    Update op_adicao.py

--- 12_Funcoes_e_imports/op_math/op_adicao.py
+++ 12_Funcoes_e_imports/op_math/op_adicao.py
@@ -1,7 +1,14 @@
 def adicao(valor1: int, valor2: int) -> int:
-    # soma(a, b): apenas pode fazer ‘soma(x, 1)’ (sem somar a e b)
-    pass
-
+    if valor2 == 0:
+        return valor1
+    elif valor2 > 0:
+        for _ in range(valor2):
+            valor1 += 1
+        return valor1
+    else:  # valor2 negativo
+        for _ in range(-valor2):
+            valor1 -= 1
+        return valor1
 
 assert adicao(1, 2) == 3
 assert adicao(1, 0) == 1
