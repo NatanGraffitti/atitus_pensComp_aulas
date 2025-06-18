35121e57f5f1ea4982d1ed55c1abf92b8bdf1a5d
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-06-18 14:48:18 -0300

    Update soma_dois.py

--- 17_TDD/soma_dois.py
+++ 17_TDD/soma_dois.py
@@ -1,10 +1,15 @@
 def soma_dois(lista: list, alvo: int) -> list:
-    pass
+    lista_soma = []
+    soma = 0
+    for i in lista:
+        if i  + j = alvo:
+            lista_soma.append(i
 
 
-assert soma_dois([2, 7, 11, 15], 9) == [0, 1]  # l[0]+l[1] == 9
-assert soma_dois([3, 2, 4], 6) == [1, 2]
-assert soma_dois([3, 3], 6) == [0, 1]
-assert soma_dois([], 6) is None
-assert soma_dois([1], 6) is None
-assert soma_dois([1, 2], 6) is None
+def test():
+    assert soma_dois([2, 7, 11, 15], 9) == [0, 1]  # l[0]+l[1] == 9
+    assert soma_dois([3, 2, 4], 6) == [1, 2]
+    assert soma_dois([3, 3], 6) == [0, 1]
+    assert soma_dois([], 6) is None
+    assert soma_dois([1], 6) is None
+    assert soma_dois([1, 2], 6) is None
