6ae3043824f4ffd3f1dbdad81e9a1303153ba22b
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2025-06-18 14:57:14 -0300

    Update test_soma_digitos.py

--- 17_TDD/test_soma_digitos.py
+++ 17_TDD/test_soma_digitos.py
@@ -1,11 +1,26 @@
+def soma_str(param: str) -> int:
+    """
+    Soma todos os dígitos numéricos encontrados em uma string,
+    ignorando outros caracteres e sinais.
 
-def soma_str(param):
-    return 0
+    Args:
+        param (str): A string de entrada que pode conter dígitos e outros caracteres.
 
+    Returns:
+        int: A soma de todos os dígitos encontrados na string.
+    """
+    total_sum = 0
+    for char in param:
+        if char.isdigit():
+            total_sum += int(char)
+    return total_sum
 
-# assert soma_str("") == 0
-# assert soma_str("a") == 0
-# assert soma_str("4") == 4
-# assert soma_str("5ab6") == 11  # 5+6
-# assert soma_str("3 -4 z5") == 12  # 3+4+5, ignore o sinal
-# assert soma_str("11a2z3") == 7  # 1+1+2+3
+def test():
+    assert soma_str("") == 0
+    assert soma_str("a") == 0
+    assert soma_str("4") == 4
+    assert soma_str("5ab6") == 11  # 5+6
+    assert soma_str("3 -4 z5") == 12  # 3+4+5, ignora o sinal e o espaço
+    assert soma_str("11a2z3") == 7  # 1+1+2+3
+
+print("Todos os testes passaram com sucesso!")
